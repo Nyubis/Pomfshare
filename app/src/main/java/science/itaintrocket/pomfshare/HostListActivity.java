ef56538312a8abcc8563c553d628fe3a3692c3af
Roel <nyubis@gmail.com>
2016-04-21 15:45:09 +0200

    Read hosts from database instead of array

--- app/src/main/java/science/itaintrocket/pomfshare/HostListActivity.java
+++ app/src/main/java/science/itaintrocket/pomfshare/HostListActivity.java
@@ -2,14 +2,19 @@ package science.itaintrocket.pomfshare;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CursorAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.SimpleCursorAdapter;
 
 public class HostListActivity extends ListActivity {
 	private Host[] hosts;
@@ -18,32 +23,32 @@ public class HostListActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 
-		// This should probably be stored in a proper format at some point, but cba now
-		hosts = new Host[14];
-		hosts[0] = new Host("Uguu", "https://uguu.se/api.php?d=upload-tool", "100MiB, 24 hours", Host.Type.UGUU);
-		hosts[1] = new Host("Mixtape.moe", "https://mixtape.moe/upload.php?output=gyazo", "100MiB", Host.Type.POMF);
-		hosts[2] = new Host("pomf.io", "https://pomf.io/upload.php?output=gyazo", "256MiB", Host.Type.POMF);
-		hosts[3] = new Host("Maxfile", "https://maxfile.ro/static/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
-		hosts[4] = new Host("SICP", "http://sicp.me/", "25MiB", Host.Type.UGUU);
-		hosts[5] = new Host("1339", "http://1339.cf/upload.php?output=gyazo", "100MiB", Host.Type.POMF);
-		hosts[6] = new Host("pantsu.cat", "https://pantsu.cat/upload.php?output=gyazo", "128MiB", Host.Type.POMF);
-		hosts[7] = new Host("Pomf.cat", "http://pomf.cat/upload.php?output=gyazo", "50MiB, Work in progress", Host.Type.POMF);
-		hosts[8] = new Host("up.che.moe", "http://up.che.moe/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
-		hosts[9] = new Host("g.zxq.co", "http://g.zxq.co/upload.php?output=gyazo", "80MiB", Host.Type.POMF);
-		hosts[10] = new Host("openhost.xyz", "http://openhost.xyz/upload.php?output=gyazo", "1024MiB", Host.Type.POMF);
-		hosts[11] = new Host("pomf.pl", "http://pomf.pl/upload.php?output=gyazo", Host.Type.POMF);
-		hosts[12] = new Host("pomf.hummingbird.moe", "http://pomf.hummingbird.moe/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
-		hosts[13] = new Host("pomf.lesderid.net", "https://pomf.lesderid.net/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
-
-		ListAdapter adapter = new HostArrayAdapter(this, hosts);
-
+		HostDbHelper helper = new HostDbHelper(getApplicationContext());
+		SQLiteDatabase db = helper.getReadableDatabase();
+		String[] cols = {
+				HostDbContract.HostTable._ID,
+				HostDbContract.HostTable.COL_HOST_NAME,
+				HostDbContract.HostTable.COL_HOST_URL,
+				HostDbContract.HostTable.COL_HOST_DESC,
+				HostDbContract.HostTable.COL_HOST_API,
+		};
+		Cursor c = db.query(HostDbContract.HostTable.TABLE_NAME, cols, null, null, null, null, null);
+		ListAdapter adapter = new SimpleCursorAdapter(this,	android.R.layout.two_line_list_item, c,
+				new String[]{ HostDbContract.HostTable.COL_HOST_NAME,
+						      HostDbContract.HostTable.COL_HOST_DESC },
+				new int[]{ android.R.id.text1, android.R.id.text2 },
+				CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 		setListAdapter(adapter);
 	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
+		SQLiteCursor cur = (SQLiteCursor)getListView().getItemAtPosition(position);
 		Intent data = new Intent();
-		data.putExtra("Host", hosts[position].toBundle());
+		Host selected = new Host(cur.getString(1), cur.getString(2), cur.getString(3),
+				Host.Type.valueOf(cur.getString(4)));
+
+		data.putExtra("Host", selected.toBundle());
 		setResult(RESULT_OK, data);
 		super.finish();
 	}
