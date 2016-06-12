250cc716c2855a1628e26c6b4d08d02f786e7186
Eliot Whalan <ewhal@pantsu.cat>
2016-06-12 22:51:11 +1000

    Fix invalid host type

--- app/src/main/java/science/itaintrocket/pomfshare/HostListActivity.java
+++ app/src/main/java/science/itaintrocket/pomfshare/HostListActivity.java
@@ -35,8 +35,8 @@ public class HostListActivity extends ListActivity {
 		hosts[16] = new Host("qt.cx", "http://qt.cx/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
 		hosts[17] = new Host("reich.io", "http://reich.io/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
 		hosts[18] = new Host("SICP", "http://sicp.me/", "25MiB", Host.Type.UGUU);
-		hosts[19] = new Host("steamy.moe", "https://steamy.moe/upload.php?output=gyazo", "512MiB", Host.Type.UGUU);
-		hosts[19] = new Host("sugoi.vidyagam.es", "https://sugoi.vidyagam.es/upload.php?output=gyazo", "100MiB", Host.Type.UGUU);
+		hosts[19] = new Host("steamy.moe", "https://steamy.moe/upload.php?output=gyazo", "512MiB", Host.Type.POMF);
+		hosts[19] = new Host("sugoi.vidyagam.es", "https://sugoi.vidyagam.es/upload.php?output=gyazo", "100MiB", Host.Type.POMF);
 		hosts[20] = new Host("Uguu", "https://uguu.se/api.php?d=upload-tool", "100MiB, 24 hours", Host.Type.UGUU);
 		hosts[21] = new Host("up.asis.io", "http://up.asis.io/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
 		hosts[22] = new Host("up.che.moe", "http://up.che.moe/upload.php?output=gyazo", "50MiB", Host.Type.POMF);
