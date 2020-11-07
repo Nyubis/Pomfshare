5e4274f93c8f53fea0a19f2ca266f9b2904d2317
Nyubis <nyubis@gmail.com>
2020-11-07 10:01:57 +0100

    Fix mimetype/extension issue

--- app/src/main/kotlin/science/itaintrocket/pomfshare/Uploader.kt
+++ app/src/main/kotlin/science/itaintrocket/pomfshare/Uploader.kt
@@ -3,6 +3,7 @@ package science.itaintrocket.pomfshare
 import android.net.Uri
 import android.os.AsyncTask
 import android.util.Log
+import android.webkit.MimeTypeMap
 import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -14,15 +15,18 @@ class Uploader(private val source: MainActivity, private val contentUri: Uri, pr
     private val client = OkHttpClient()
 
     override fun doInBackground(vararg params: String?): String {
-        val filename = params[0]
-        val contentType = params[1]
+        val filename = params[0]!!
+        val contentType = params[1]!!
+        val uploadFilename = addExtension(filename, contentType)
         val uploadUrl = host.url
         val fieldName = if (host.type === Host.Type.POMF) "files[]" else "file"
+        Log.d(tag, "filename: '${filename}', contentType: '${contentType}', uploadFilename: '${uploadFilename}', "
+                + "uploadUrl: '${uploadUrl}', fieldName: '${fieldName}'")
         val result: String
         try {
             val formBodyBuilder = MultipartBody.Builder()
                     .setType(MultipartBody.FORM)
-                    .addFormDataPart(fieldName, filename, ContentUriRequestBody(source.contentResolver, contentUri))
+                    .addFormDataPart(fieldName, uploadFilename, ContentUriRequestBody(source.contentResolver, contentUri))
             if (host.authRequired == true) {
                 host.authKey?.let { formBodyBuilder.addFormDataPart("uuid", it) }
             }
@@ -48,6 +52,17 @@ class Uploader(private val source: MainActivity, private val contentUri: Uri, pr
         return if (index > 0) {
             result.substring(index)
         } else result
+    }
+
+    // Due to how the sharing works, the filename may not include an extension, but some of the hosts rely on the
+    // extension to infer mimetype, so if our file doesn't have an extension, we add one that's appropriate for the
+    // mimetype.
+    private fun addExtension(filename: String, contentType: String): String {
+        val extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(contentType)
+        if (extension != null && filename.endsWith(extension, ignoreCase = true)) {
+            return filename
+        }
+        return "$filename.$extension"
     }
 
     override fun onPostExecute(result: String) {
